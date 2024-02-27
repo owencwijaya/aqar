0af17a2a07e8b7b2c03cad9140a41aa2dd4787f1
patrickamadeus <patrickai0309@gmail.com>
2024-02-27 21:36:30 +0700

    threading send locs

--- object_detection/main.py
+++ object_detection/main.py
@@ -1,7 +1,8 @@
 import cv2 as cv
-from time import time
+from time import time, sleep
 import argparse
 import os
+import threading  # Import the threading module
 
 from windowcapture import MacOSWindowCapture, WindowCapture
 from vision import Vision
@@ -22,10 +23,42 @@ wincap = MacOSWindowCapture(window_name=args.window_name)
 # wincap = WindowCapture(window_name=args.window_name)
 
 # Needle Target
-vision_humanoid = Vision(f'{COMPONENT_PATH}/humanoid_1.jpg')
-vision_humanoid.init_control_gui()
+vision_needles = []
+for file in os.listdir(f'{COMPONENT_PATH}'):
+    if file.endswith('.jpg'):
+        vision_needles.append(Vision(f'{COMPONENT_PATH}/{file}'))
+vision_humanoid = Vision(f'{COMPONENT_PATH}/1.jpg')
+# vision_humanoid.init_control_gui()
+
 # HSV Filter
 hsv_filter = HsvFilter(0, 180, 129, 15, 229, 243, 143, 0, 67, 0)
+
+# Function to print rectangles and points every 5 seconds
+def emit_rectangles_and_points():
+    while True:
+        ids = []
+        recs = []
+        points = []
+
+        # Get the points
+        for vision_needle in vision_needles:
+            rectangles = list([list(i) for i in vision_needle.find(wincap.get_screenshot(), threshold=0.35, debug_mode='complete')])
+            points = list(vision_needle.get_click_points(rectangles))
+            id = [vision_needle.get_id()] * len(points)
+
+            if id:
+                ids += id
+                recs += list(rectangles)
+                points += list(points)
+
+        res = list(zip(ids, recs, points))
+        print(res)
+        # Sleep for 5 seconds
+        sleep(5)
+
+# Create a thread for the background process
+background_thread = threading.Thread(target=emit_rectangles_and_points)
+background_thread.start()
 
 loop_time = time()
 while True:
@@ -33,7 +66,7 @@ while True:
     screenshot = wincap.get_screenshot()
 
     # Preprocess image
-    processed_image = vision_humanoid.apply_hsv_filter(screenshot, hsv_filter)
+    # processed_image = vision_humanoid.apply_hsv_filter(screenshot, hsv_filter)
 
     # Get the points
     rectangles = vision_humanoid.find(screenshot, threshold=0.35, debug_mode='complete')
@@ -44,13 +77,11 @@ while True:
     output_image = vision_humanoid.draw_crosshairs(output_image, points)
 
     # display the processed image
-    cv.imshow('Processed', processed_image)
     cv.imshow('Matches', output_image)
 
     # Calculate the FPS
     print('FPS: {}'.format(1 / (time() - loop_time)))
     loop_time = time()
-
 
     # Exit
     if cv.waitKey(1) & 0xFF == ord('q'):
